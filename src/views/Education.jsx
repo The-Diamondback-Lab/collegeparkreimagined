dd135bdb42bc13fad9d25ed05c6b8234c8ae7afb
Akshay Guthal <akguthal@gmail.com>
2018-05-20 00:05:01 -0400

    added ids to sections

---
+++ src/views/Education.jsx
@@ -0,0 +1,125 @@
+//react imports
+import React, { Component } from 'react';
+
+const Education = () => {
+  return (
+    <main className="story" id="education">
+      <div className="wrapper" id="main-wrapper">
+        <h1>EDUCATION</h1>
+        <h3 class="first-line">
+          “When I came here in 2010, I knew nothing about K-12 education, nothing about revitalizing an area -- zip, zero,” said President Loh.  But soon after, he learned that the two went hand-in-hand.
+          <br /><br />
+          <span class = "black">
+            After his first meeting with U3 Advisors LLC CEO Omar Blaik, Loh realized that the university would attract more families and professionals to the area if it worked to make the local schools more competitive relative to the rest of the state. As a first step, similar to the University of Pennsylvania’s Penn Alexander Charter School, the University of Maryland created its own blended learning middle and high school designed to prepare students for college. The school enrolls students based on a highly competitive lottery system.
+            <br /><br />
+              Executive Director Bernadette Ortiz-Brewster said many variables have contributed to the school’s growth and success since it opened in 2013.
+            </span>
+            <br /><br />
+              “One of the most exciting parts is that [University of Maryland] students come in and propose their own courses and programs,” she said. In that way, “it’s a different curriculum than what else is being offered.”
+            <br /><br />
+            <span class = "black">
+              But critics have voiced the need for additional competitive schooling options, noting that the lack of programs like College Park Academy drive them away from College Park.
+            </span>
+            <br /><br />
+              “As somebody with a one year old, it is difficult to find, when you compare yourself to other, large state schools.” said resident Mark Fuge. “I can usually convince [potential homebuyers] of all the other things, but it always comes back down to, yeah but the public schools in Montgomery County or Howard County are better.”
+        </h3>
+        <h2>ACCOMPLISHMENTS TO DATE</h2>
+        <ul>
+          <li>
+            UMD Partnership with Paint Branch Elementary School provided the first Chinese Language instruction for elementary students in Prince George’s County
+            <ul>
+              <li>
+                According to Sen. Rosapepe, the university is especially interested in <b>supporting and promoting</b> dual immersion curricula in local schools, especially Chinese and Spanish language
+              </li>
+              <li>
+                As per the 2012-2013 academic school year annual report summary, The UMD-Paint Branch Elementary School partnership “expos[es] Paint Branch students to the opportunities and expertise of the university, a broader global perspective, and 21st century life and business skills relevant for building a successful future.”
+              </li>
+            </ul>
+          </li>
+          <li>
+            College Park Academy, a rigorous college prep middle and high school, opened 2013 and moved into a brand new building in the Discovery District in fall 2017.
+            <ul>
+              <li>
+                According to the PGCPS website, <b>the academy has produced some of the top test scores in the state of Maryland</b>, outperforming not just the county and state but also Howard and Montgomery County Schools in some areas. Next year the academy will graduate its first senior class.
+              </li>
+              <li>
+                Loh attributes the school’s success to “the huge numbers of University of Maryland students who go and tutor and mentor them.” He also schools from all over Prince George’s County have expressed interest in replicating portions of the CPA model in their schools.
+              </li>
+              <li>
+                “And that’s one of the things we’re exploring,” Loh said, “in fact, we’re having a conversation with an organization to take College Park Academy to other parts of the state.”
+              </li>
+              <li>
+                With the addition of a new local boundary area for College Park Academy, students living in the jurisdictions of six area elementary schools will comprise at least 35 percent of the students admitted: Berwyn Heights, Cherokee Lane, Hollywood, Paint Branch, Riverdale and University Park. Olson said this should provide assurance to local families applying to College Park Academy.
+              </li>
+              <li>
+                Next year, the school will enroll up to 675 students, said Executive Director Bernadette Ortiz-Brewster. She said some 1,675 children are on the waitlist.
+              </li>
+              <li>
+                Olson said the the “new sibling preference” usually works to enroll siblings of current students to help parents feel more assured with the lottery system.
+              </li>
+              <li>
+                Promoting the expanded boundary area is included on the partnership’s 2018 work plan.
+              </li>
+            </ul>
+          </li>
+          <li>
+            New Childcare and Pre-K coming to College Park. UMD-backed daycare is set to move into the Calvert Road School site. Another provider is locating in North College Park.
+            <ul>
+              <li>
+                Olson said two new programs for young children are expected to launch in 2019. He said the university currently has just one daycare center for staffers, which has waiting list.
+              </li>
+              <li>
+                The daycare center will be placed a few blocks from City Hall in the old and mostly vacant former elementary school on Calvert Road, Olson said.
+              </li>
+              <li>
+                The College Park early learning center will be open for ages 3-5. 250,000 in state funding is in place.
+              </li>
+            </ul>
+          </li>
+          <li>
+            K-12 Schools Survey completed
+            <ul>
+              <li>
+                According to Olson, the survey examined what local parents want out of Prince George’s County Public Schools. He said the results will be used to help inform lawmakers and policymakers.
+              </li>
+              <li>
+                He said the results of this study are still being compiled.
+              </li>
+              <li>
+                Loh said a systemic transformation of the local school system will “take years and years” to accomplish but additions like College Park Academy “is a reason why we’re seeing an uptick in faculty and staff living here.”
+              </li>
+              <li>
+                The partnership will implement strategies based on the survey and present data to lawmakers, according to the 2018 work plan. Leaders will also “catalog, package and promote local College Park schools” by creating a brochure and a “one-stop shop” webpage for interested parents.
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+      <h2>ADDITIONAL HIGHLIGHTS OF THE 2018 WORK PLAN</h2>
+        <ul>
+          <li>
+            <b>Explore a K-5 school.</b> Explore adding a K-5, junior CPA in College Park with CPA, the Children’s Guild and others, informed by the survey. Partnership leading and supporting.
+            <ul>
+              <li>
+                According to Olson, “it’s definitely something that’s being looked into” but will not happen immediately.
+              </li>
+              <li>
+                There is no estimated opening date.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>Before and After School Programs.</b> The Partnership will explore increasing options for before and after-school programs for College Park students.
+            <ul>
+              <li>
+                Olson said while there are no formal plans for this venture or an estimation of its monetary value, he hopes to solidify logistics soon. He said these programs will hopefully involve “learning in a fun way” and allow students with space and assistance to do homework, as well as offer other “creative things” such as art, drama and music.
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Education;
